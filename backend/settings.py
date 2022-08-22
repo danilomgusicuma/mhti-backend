e5a4980cdead462823e1a9804168028c6bcb2758
Danilo Miranda Gusicuma <danilo.m.gusicuma@gmail.com>
2022-08-22 13:41:44 +0200

    setting things up to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -121,7 +121,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
